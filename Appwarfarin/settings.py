ff4e9df70e4bb68e0da32841c7760d5f61d16292
SusanMageto <susan.mageto@student.moringaschool.com>
2022-07-06 22:45:56 +0300

    fixes tokens

--- Appwarfarin/settings.py
+++ Appwarfarin/settings.py
@@ -140,9 +140,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAdminUser',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication', 
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication', 
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     )
 }
 
